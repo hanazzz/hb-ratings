ecb93e245ee765be30d0ea2c867206502708e8bf
Hana Zait <hana.o.zait@gmail.com>
2022-09-08 12:33:56 -0700

    Added crud.py and finished create_user()

---
+++ crud.py
@@ -0,0 +1,24 @@
+"""CRUD operations."""
+
+from model import db, User, Movie, Rating, connect_to_db
+
+
+# Functions start here!
+
+
+def create_user(email, password):
+    """Create and return a new user."""
+
+    user = User(email=email, password=password)
+
+    return user
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
